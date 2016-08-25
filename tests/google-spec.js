d79228e316856bad5b1c76dd7a178a52f1cb6e0f
Siva Kandaraj <siva.kandaraj@gmail.com>
2016-08-25 15:27:50 +1000

    Init

---
+++ tests/google-spec.js
@@ -0,0 +1,22 @@
+module.exports = {
+    'Demo test Google' : function (browser) {
+        browser
+            .url('http://localhost:8080')
+            .waitForElementVisible('body', 1000)
+            .assert.hidden("table")
+            .click('.search-panel-button')
+            .setValue('input[type=text]', '10 Lucknow street')
+            .pause(2000)
+            .keys(browser.Keys.DOWN_ARROW)
+            .keys(browser.Keys.ENTER)
+            .waitForElementVisible('table', 3000)
+            .assert.visible("table")
+            .assert.visible(".leaflet-clickable")
+            .click(".leaflet-clickable > i")
+            .pause(2000)
+            .getTagName("table", function(result) {
+                console.log(result);
+            })
+            .end();
+    }
+};
